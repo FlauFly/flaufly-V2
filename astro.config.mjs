a78a01ad36c4a0701d048f377d4b179c1cb4d4f4
FlauFly <tirhhe@gmail.com>
2025-07-10 09:47:56 +0200

    navbar created

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
-
 import mdx from "@astrojs/mdx";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://flaufly.com",
-  integrations: [preact(), mdx()]
+  integrations: [mdx(), react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
