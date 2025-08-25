43a3fddaec2181208fe3825781ea311a96b804c0
FlauFly <tirhhe@gmail.com>
2025-08-25 09:21:49 +0200

    Changed name from tags to topics in whole website

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,16 +4,15 @@ import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 // Define a `loader` and `schema` for each collection
 const blog = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/blog" }),
-    schema: z.object({
-      title: z.string(),
-      description: z.string(),
-      startDate: z.date(),
-      updated: z.date(),
-      tags: z.array(z.string()),
-      topics: z.array(z.string()),
-      writingStage: z.string()
-    })
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/blog" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    startDate: z.date(),
+    updated: z.date(),
+    topics: z.array(z.string()),
+    writingStage: z.string(),
+  }),
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = { blog };
