d8c3e79dd09c68adb55ff901fd71464232e6fa73
FlauFly <tirhhe@gmail.com>
2025-10-30 23:41:09 +0100

    Split of travel articles into daily entries

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,18 +12,37 @@ const blog = defineCollection({
     updated: z.date(),
     topics: z.array(z.string()),
     writingStage: z.string(),
+    toc: z.boolean().optional(),
   }),
 });
 
-const travels = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/travels" }),
+const destinations = defineCollection({
+  loader: glob({
+    pattern: "**/[^_]*.{md,mdx}",
+    base: "./src/content/destinations/",
+  }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
       startDate: z.date(),
       endDate: z.date(),
       cover: image().optional(),
+      toc: z.boolean().optional(),
     }),
 });
+
+const travelogues = defineCollection({
+  loader: glob({
+    pattern: "**/[^_]*.{md,mdx}",
+    base: "./src/content/travelogues/",
+  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      travel: z.string().optional(),
+    }),
+});
+
 // Export a single `collections` object to register your collection(s)
-export const collections = { blog, travels };
+export const collections = { blog, destinations, travelogues };
