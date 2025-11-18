ea21712803cc3d5a30a3bd1fd1b52bb714c6a1fe
FlauFly <tirhhe@gmail.com>
2025-11-18 11:18:01 +0100

    Renaming of collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,20 +3,35 @@ import { glob } from "astro/loaders";
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 // Define a `loader` and `schema` for each collection
-const blog = defineCollection({
+const essaysCollection = defineCollection({
   loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/essays" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
     startDate: z.date(),
     updated: z.date(),
+    type: z.literal("essay"),
     topics: z.array(z.string()),
     writingStage: z.string(),
     toc: z.boolean().optional(),
   }),
 });
 
-const destinations = defineCollection({
+const notesCollection = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/notes" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    startDate: z.date(),
+    updated: z.date(),
+    type: z.literal("notes"),
+    topics: z.array(z.string()),
+    writingStage: z.string(),
+    toc: z.boolean().optional(),
+  }),
+});
+
+const destinationsCollection = defineCollection({
   loader: glob({
     pattern: "**/[^_]*.{md,mdx}",
     base: "./src/content/destinations/",
@@ -26,12 +41,13 @@ const destinations = defineCollection({
       title: z.string(),
       startDate: z.date(),
       endDate: z.date(),
+      type: z.literal("destination"),
       cover: image().optional(),
       toc: z.boolean().optional(),
     }),
 });
 
-const travelogues = defineCollection({
+const traveloguesCollection = defineCollection({
   loader: glob({
     pattern: "**/[^_]*.{md,mdx}",
     base: "./src/content/travelogues/",
@@ -40,10 +56,16 @@ const travelogues = defineCollection({
     z.object({
       title: z.string(),
       date: z.date(),
+      type: z.literal("travelogue"),
       travel: z.string().optional(),
       location: z.string().optional(),
     }),
 });
 
 // Export a single `collections` object to register your collection(s)
-export const collections = { blog, destinations, travelogues };
+export const collections = {
+  essaysCollection,
+  notesCollection,
+  destinationsCollection,
+  traveloguesCollection,
+};
